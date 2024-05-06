a49e19ce544668b862bfef493d05f7acbc347036
Srijal Shrestha <srijal@GEONET.COM>
2024-05-06 12:53:45 +0545

    Hello World File Upload

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MaterialApp(home: Text('Hello World!')));
+}
+
+
+// Check out the flutter widget catalog. https://docs.flutter.dev/ui/widgets
+// That site can help u learn about some of the most important widgets offered
+// by flutter
+
+
